00c89ea516cdb1895d7070cb5ab0637825746749
Ibrahim-Lokman <ibrahimlokman.bd@gmail.com>
2024-03-20 02:53:43 +0600

    Service class implementation and added to controller.

---
+++ src/messages/messages.service.ts
@@ -0,0 +1,22 @@
+import { MessagesRepository } from "./messages.repository";
+
+
+export class MessagesService {
+    messagesRepository: MessagesRepository;
+    constructor() {
+        this.messagesRepository = new MessagesRepository();
+    }
+
+    async findOne(id: string) {
+        return this.messagesRepository.findOne(id);
+    }
+
+    async findAll() {
+        return this.messagesRepository.findAll();
+    }
+
+    async create(content: string) {
+        return this.messagesRepository.create(content);
+    }
+
+}
