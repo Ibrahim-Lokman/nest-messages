c062c204fb53049e9a17fd0160aae264872999af
Ibrahim-Lokman <ibrahimlokman.bd@gmail.com>
2024-03-20 02:59:19 +0600

    Error handling on controller.

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, NotFoundException, Param, Post } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
 import { MessagesService } from './messages.service';
 
@@ -21,8 +21,12 @@ export class MessagesController {
     }
 
     @Get('/:id')
-    getMessage(@Param('id') id: string) {
+    async getMessage(@Param('id') id: string) {
         console.log(id);
-        return this.messagesService.findOne(id);
+        const message = await this.messagesService.findOne(id);
+        if (!message) {
+            throw new NotFoundException('message not found');
+        }
+        return message;
     }
 }
